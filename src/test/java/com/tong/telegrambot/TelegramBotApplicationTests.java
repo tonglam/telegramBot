a93f951c31812be10dc6d4dc733c53179766e4ab
tong <oncwnuOh0fwZryEHNHFQW2PNJYQI@git.weixin.qq.com>
2022-08-22 14:26:55 +0800

    Initial commit

---
+++ src/test/java/com/tong/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tong.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
